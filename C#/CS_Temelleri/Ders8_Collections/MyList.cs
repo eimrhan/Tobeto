6059577e103b6d135ef0f03cda645b3b671f672e
Emirhan Çelebi <emirhan@celebi.dev>
2023-11-09 16:42:45 +0300

    Collections, Generics, Constructor

---
+++ C#/CS_Temelleri/Ders8_Collections/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders8_Collections
+{
+    class MyList<T> // Type'ın T'si. Belirli bir tip de belirlenebilir fakat bu şekilde oluna daha dinaik oluyor.
+    {
+        T[] items; // T tipinde items isimli bir array oluşturduk.
+        public MyList() // Constructor (ctor->tab) // Class ile aynı isim olur.
+        {
+            // items newlendiğinde Constructor bloğu otomatik olarak çalışır. bu yüzden burada
+            items = new T[0]; // ile sıfır elemanlı diziyi oluşturuyoruz.
+        }
+        public void Add(T item) // kullanıcı hangi tipte veri girerse o tipi alır, gocunmaz.
+        {
+            // yeni eleman eklerken öncelikle boyutunu 1 artırmamız lazım, fakat onun da öncesinde // 2.
+            // yeni dizi oluşturunca referans adresi değişeceği için eski verileri kaybediyorduk, 
+            // bunun için eski elemanları önce bir geçici diziye atıyoruz. // 1.
+            // sonra döngüyle eski elemanları geri alıyoruz. // 3.
+            // ve son olarak yeni elemanı yeni dizinin son elemanı olarak atıyoruz. // 4.
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+                items[i] = tempArray[i];
+            items[items.Length-1] = item;
+        }
+
+        public int Length // items'ın Length'ine erişmek için oluşturduğumuz metot.
+        {
+            get { return items.Length; }
+        }
+        public T[] Items // elemanlara erişmek için
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ C#/CS_Temelleri/Ders8_Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using Ders8_Collections;
+
+string[] names = new string[] { "Emir", "Han" };
+// 2 elemanlı bir string dizisi.
+
+//names[2] = "isim"; // 3. elemanı atayamazsın, uygulama patlar.
+//Console.WriteLine(names[2]);
+
+names = new string[3]; // aynı isimle 3 elemanlı yeni bir string dizisi oluştu
+names[2] = "isim"; // 3. elemana veriyi atar fakat
+Console.WriteLine(names[2]); // bu artık yeni bir adresi işaret ettiği için eski verileri kaybettik
+Console.WriteLine(names[1]); // boş döner.
+
+
+Console.WriteLine("******************************************");
+//* İşte burada Listeler devreye giriyor.
+
+//List<string> names2 = new List<string>();
+List<string> names2 = new List<string> { "Emir", "Han" };
+
+names2.Add("isim");
+Console.WriteLine(names2[2]); // şimdi sıkıntı çıkmadı işte.
+
+
+Console.WriteLine("******************************************");
+//* Listeler olmasaydı bu generic yapıyı kendimiz nasıl oluştururduk:
+//** Generics - (MyList.cs dosyasına göz at.)
+
+MyList<string> names3 = new MyList<string>();
+
+names3.Add("Emir");
+Console.WriteLine(names3.Length);
+names3.Add("Han");
+Console.WriteLine(names3.Length);
+
+foreach (string name in names3.Items)
+{
+    Console.WriteLine(name);
+}
