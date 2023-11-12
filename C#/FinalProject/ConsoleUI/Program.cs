fe5100b40e11f5b149395157cb63edc4b89e7c65
Emirhan Çelebi <emirhan@celebi.dev>
2023-11-13 00:30:19 +0300

    Kurumsal Mimari: Aşama 2
    EntityFramework

--- C#/FinalProject/ConsoleUI/Program.cs
+++ C#/FinalProject/ConsoleUI/Program.cs
@@ -14,11 +14,28 @@
 /* ************************************** */
 
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+//ProductManager productManager = new ProductManager(new InMemoryProductDal());
+ProductManager productManager = new ProductManager(new EfProductDal());
+// S'O'LID - O : Open Closed Principle ("projene yeni bir şey ekliyorsan, mevcut kodlara dokunamazsın.")
+// Kodlarda değişim yapmadan sadece InMemoryProductDal'ı EfProductDal olarak değiştirerek yeni db'e geçtik.
 
 foreach (var product in productManager.GetAll())
 {
     Console.WriteLine(product.ProductName);
 }
+Console.WriteLine("--------------");
+
+foreach (var product in productManager.GetAllByCategoryID(2))
+{
+    Console.WriteLine(product.ProductName);
+}
+Console.WriteLine("--------------");
+
+foreach (var product in productManager.GetByUnitPrice(50,100))
+{
+    Console.WriteLine(product.ProductName);
+}
+Console.WriteLine("--------------");
