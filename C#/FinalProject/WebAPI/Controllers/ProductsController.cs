361057bd677f004eb86db49fa6438b7796230af2
Emirhan Çelebi <emirhan@celebi.dev>
2023-11-19 14:59:12 +0300

    WebAPI

---
+++ C#/FinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // Attribute
+    public class ProductsController : ControllerBase
+    {
+        // herhangi somut bir sınıftan newleme yapmayarak bağımlılığı azaltıyoruz.
+        // (IProductService, Manager'ın referansını tutuyor.)
+        // Loosely coupled - Gevşek bağlılık
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
---
+++ C#/FinalProject/WebAPI/Program.cs
@@ -0,0 +1,40 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// IoC Container - Inversion of Control - Deðiþimin Kontrolü :
+builder.Services.AddSingleton<IProductService, ProductManager>();
+// Constructor'da IProductService isteyen bir baðýmlýlýk görürse arkaplanda onun yerine ProductManager new'lemesi yapar.
+// Singleton tüm bellekte sadece 1 tane ProductManager tutar, istediði kadar client gelsin, hepsine ayný instance'ý verir.
+// Hepsi ayný referansý tutar. (tabi bunu sadece "içerisinde data tutmadýðýn" durumlarda kullanabilirsin.)
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
+// aynýsýný IProductDal baðýmlýlýklarý için de yapýyoruz.
+
+// AOP - Aspect Oriented Programming
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
