9244bbb409c2647e8dd8ce7128efa0473ae4e71f
Emirhan Çelebi <emirhan@celebi.dev>
2023-11-21 21:11:56 +0300

    Validation

--- C#/FinalProject/Business/Concrete/ProductManager.cs
+++ C#/FinalProject/Business/Concrete/ProductManager.cs
@@ -1,10 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,9 +33,12 @@ namespace Business.Concrete
             // business codes ...
             // iş kodlarından geçerse sıra veri erişiminde. // DataAccess
 
-            if (product.ProductName.Length < 2)
+            var context = new ValidationContext<Product>(product);
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(context);
+            if(!result.IsValid)
             {
-                return new ErrorResult(Messages.ProductNameInvalid);
+                throw new ValidationException(result.Errors);
             }
 
             _productDal.Add(product);
---
+++ C#/FinalProject/Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            //RuleFor(p => p.ProductName).NotEmpty().MinimumLength(2);
+            // bitişik kurallar yazılabilir. ama ayrı yazmak SOLID'e daha uygun.
+            RuleFor(p => p.ProductName).MinimumLength(2);
+
+            // RuleFor(p => p.UnitPrice > 0);
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10)
+                .When(p => p.CategoryID == 1)   // alt durum ekleyebiliriz.
+                .WithMessage("Kategori ID'si 1 olan ürünün fiyatı en az 10 TL olmalı.");
+
+            RuleFor(p => p.ProductName).Must(StartWithA) // StartWithA kendi oluşturduğumuz özel metot.
+                .WithMessage("Ürün ismi 'A' harfi ile başlamalı.");
+                // default mesajlar yerine özel mesaj eklenebilir. (özel metotlarda kullanılması iyi olur.)
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
