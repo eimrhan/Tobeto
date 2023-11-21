f0f1b8444b8bff608cb9b348734bc7ee26a50250
Emirhan Çelebi <emirhan@celebi.dev>
2023-11-21 20:02:11 +0300

    Autofac

---
+++ C#/FinalProject/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            // bu iki kod WebAPI/Program.cs'de yazdığımız aşağıdaki kodların görevini üstleniyor:
+            // builder.Services.AddSingleton<IProductService, ProductManager>();
+            // builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+            // burada bu yapıyı daha base bir seviyeye çekmiş olduk.
+            // artık bir birkaç basit işlem ile autofac yapısından vazgeçip başka bir yapıya kolaylıkla geçebiliriz.
+        }
+    }
+}
--- C#/FinalProject/WebAPI/Program.cs
+++ C#/FinalProject/WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -9,11 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // IoC Container - Inversion of Control - Deðiþimin Kontrolü :
-builder.Services.AddSingleton<IProductService, ProductManager>();
+//// builder.Services.AddSingleton<IProductService, ProductManager>();
 // Constructor'da IProductService isteyen bir baðýmlýlýk görürse arkaplanda onun yerine ProductManager new'lemesi yapar.
 // Singleton tüm bellekte sadece 1 tane ProductManager tutar, istediði kadar client gelsin, hepsine ayný instance'ý verir.
 // Hepsi ayný referansý tutar. (tabi bunu sadece "içerisinde data tutmadýðýn" durumlarda kullanabilirsin.)
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//// builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // aynýsýný IProductDal baðýmlýlýklarý için de yapýyoruz.
 
 // AOP - Aspect Oriented Programming
@@ -21,6 +24,10 @@ builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    { builder.RegisterModule(new AutofacBusinessModule()); });
 
 var app = builder.Build();
 
