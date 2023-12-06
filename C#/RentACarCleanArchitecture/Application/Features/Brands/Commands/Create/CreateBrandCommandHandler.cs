f65cbd64a384508b0afb03471f60b498811c9fc3
Emirhan Çelebi <emirhan@celebi.dev>
2023-12-06 21:35:04 +0300

    CleanArchitecture

--- C#/RentACarCleanArchitecture/Application/Features/Brands/Commands/Create/CreateBrandCommandHandler.cs
+++ C#/RentACarCleanArchitecture/Application/Features/Brands/Commands/Create/CreateBrandCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,26 @@ namespace Application.Features.Brands.Commands.Create;
 
 public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
 {
-    public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    IBrandRepository _brandRepository;
+
+    public CreateBrandCommandHandler(IBrandRepository brandRepository)
     {
-        // DB'ye eklenecek.
-        return Task.FromResult(new CreatedBrandResponse { Name = request.Name });
+        _brandRepository = brandRepository;
+    }
+
+    public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        Brand brand = new Brand();
+        brand.Name = request.Name;
+        brand.Id = Guid.NewGuid();
+
+        var createdBrand = await _brandRepository.AddAsync(brand);
+
+        CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+        createdBrandResponse.Id = createdBrand.Id;
+        createdBrandResponse.Name = createdBrand.Name;
+        createdBrandResponse.CreatedDate = createdBrand.CreatedDate;
+
+        return createdBrandResponse;
     }
 }
