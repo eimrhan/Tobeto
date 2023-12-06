186a35831eacf47b43628ca717aa16318bbab6d7
Emirhan Çelebi <emirhan@celebi.dev>
2023-12-06 11:39:50 +0300

    CleanArchitecture

---
+++ C#/RentACarCleanArchitecture/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Brands.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        IMediator _mediator;
+        public BrandsController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await _mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
