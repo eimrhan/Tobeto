bd5ec6647f4938d1004ca75c9572de12ff9e5779
Emirhan Çelebi <emirhan@celebi.dev>
2023-12-27 04:28:42 +0300

    BrowserRouter moved to index.js

--- React/react-intro/src/App.jsx
+++ React/react-intro/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { Homepage } from 'pages/Homepage/Homepage';
 import ProductList from 'pages/Products/ProductList';
 import ProductDetail from 'pages/Products/ProductDetail';
@@ -11,16 +11,14 @@ import AddProduct from 'pages/Products/AddProduct';
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<NavBar />
-				<Routes>
-					<Route path="/" element={<Homepage />}></Route>
-					<Route path='/products' element={<ProductList />}></Route>
-					<Route path='/products/:id' element={<ProductDetail />}></Route>
-					<Route path='/products/add' element={<AddProduct />}></Route>
-					<Route path="*" element={<div>404: Not Found</div>}></Route>
-				</Routes>
-			</BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Homepage />}></Route>
+				<Route path='/products' element={<ProductList />}></Route>
+				<Route path='/products/:id' element={<ProductDetail />}></Route>
+				<Route path='/products/add' element={<AddProduct />}></Route>
+				<Route path="*" element={<div>404: Not Found</div>}></Route>
+			</Routes>
 		</div>
 	);
 }
